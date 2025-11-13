6c361620981602b8357d05b10fcff9595dc70fbd
ATTHAPOL\Atthapol <Atthapol>
2025-11-13 11:52:52 +0700

    add

--- components/custom/FloatingLabelForm.tsx
+++
@@ -1,4 +0,0 @@
-// Re-export split components so other modules can import from this file
-export { FloatingLabelInput } from './FloatingLabelInput';
-export { default as FloatingLabelExamples } from './FloatingLabelExamples';
-export { default } from './FloatingLabelExamples';
