e49f187e100b635aa6414142cd27dbafd6d9ee61
ATTHAPOL\Atthapol <Atthapol>
2025-12-23 11:30:21 +0700

    add

--- lib/rbac.ts
+++ lib/rbac.ts
@@ -31,7 +31,7 @@ const routeRules: RoutePermissionRule[] = [
   { pattern: /^\/companies(\/.*)?$/, required: ["menu.companies"] },
   { pattern: /^\/employee\/new$/, required: ["employee.manage"] },
   { pattern: /^\/employee(\/.*)?$/, required: ["menu.employees"] },
-  { pattern: /^\/api\/products(\/.*)?$/, required: ["product.create"] },
+  { pattern: /^\/api\/products(\/.*)?$/, required: ["menu.products"] },
   { pattern: /^\/api\/employee(\/.*)?$/, required: ["employee.manage"] },
   { pattern: /^\/api\/companies(\/.*)?$/, required: ["menu.companies"] },
 ];
