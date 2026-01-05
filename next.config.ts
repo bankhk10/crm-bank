0a68c393bf8f8a2bf696dc87e7c0288aabc5a2c4
ATTHAPOL\Atthapol <Atthapol>
2026-01-05 09:33:04 +0700

    add config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {},
+  experimental: {
+    // @ts-expect-error allowedDevOrigins is not yet in the type definition
+    allowedDevOrigins: ["9fc391f6677b.ngrok-free.app"],
+  },
 };
 
 export default nextConfig;
