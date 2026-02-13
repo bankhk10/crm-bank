616ea48ffcdd63d72debb4ca0b3149dda7c0e19c
ATTHAPOL\Atthapol <Atthapol>
2026-02-13 10:04:51 +0700

    add

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -7,16 +7,14 @@ import {
   OctagonXIcon,
   TriangleAlertIcon,
 } from "lucide-react"
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-right"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
