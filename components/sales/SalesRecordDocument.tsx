c340d1b5b09c19496838c1f938c32b01c6a0a837
ATTHAPOL\Atthapol <Atthapol>
2026-02-20 11:30:26 +0700

    add

--- components/sales/SalesRecordDocument.tsx
+++ components/sales/SalesRecordDocument.tsx
@@ -380,8 +380,8 @@ const SalesRecordDocument = React.forwardRef<
       {/* Header */}
       <div className="doc-header">
         <div className="doc-header-left">
-          {/* <p className="company-name">บริษัท เซ็นทรัล ฟู้ดส์ จำกัด</p>
-                    <p className="company-subtitle">Central Foods Co., Ltd.</p> */}
+          <p className="company-name">บริษัท เซ็นทรัล ฟู้ดส์ จำกัด</p>
+          <p className="company-subtitle">Central Foods Co., Ltd.</p>
         </div>
         <div className="doc-header-center">
           <h1 className="doc-title">ใบบันทึกการขาย</h1>
