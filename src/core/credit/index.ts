d8ec267671728a1340b4cf13cc781b9412c3a29b
ATTHAPOL\Atthapol <Atthapol>
2026-02-24 14:03:40 +0700

    add

--- src/core/credit/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Credit Module
- * Exports credit domain types
- * Note: Services will be added as they are migrated
- */
-
-// Types
-export * from "./credit.types";
--- src/core/customers/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Customers Module
- * Exports customer domain types
- * Note: Services will be added as they are migrated
- */
-
-// Types
-export * from "./customer.types";
