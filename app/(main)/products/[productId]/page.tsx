d1c2eed73017f970caf27a09c2f12ed0891b4f49
ATTHAPOL\Atthapol <Atthapol>
2026-02-27 16:59:36 +0700

    add

--- app/(main)/products/[productId]/page.tsx
+++ app/(main)/products/[productId]/page.tsx
@@ -211,7 +211,7 @@ export default function ProductDetailPage() {
   const activeImage = images[activeImageIndex];
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {/* Back link */}
         <Link
