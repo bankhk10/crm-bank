d98ea1440bf3db9169efc3a01ffd114fc9739b65
ATTHAPOL\Atthapol <Atthapol>
2026-03-06 13:26:37 +0700

    add

--- modules/sales-targets/features/form/sales-target-form.tsx
+++ modules/sales-targets/features/form/sales-target-form.tsx
@@ -402,12 +402,6 @@ export function SalesTargetForm({ mode, initialData }: SalesTargetFormProps) {
             <Card>
                 <div className="p-6">
                     <div className="text-center relative">
-                        <Link
-                            href="/sales-targets"
-                            className="absolute left-0 top-1/2 -translate-y-1/2 p-2 text-gray-500 hover:text-gray-700 transition-colors"
-                        >
-                            <ChevronLeft className="w-6 h-6" />
-                        </Link>
                         <h5 className="font-semibold text-3xl border-b pb-6">
                             {isEdit
                                 ? "แก้ไขเป้าหมายการขาย"
