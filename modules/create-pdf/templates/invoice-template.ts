43d2ea555d33cfe2e7d736a5f72c2ac270b12ba6
ATTHAPOL\Atthapol <Atthapol>
2026-03-17 10:16:31 +0700

    add

--- modules/create-pdf/templates/invoice-template.ts
+++ modules/create-pdf/templates/invoice-template.ts
@@ -250,7 +250,7 @@ export function renderInvoiceTemplate(data: InvoiceData): string {
           <h1>บริษัท คร็อพ ซายน์ จำกัด</h1>
           <h2>CROP SCIENCES CO., LTD.</h2>
           <p>เลขที่ 22 อาคารไอซี ถนนพระรามที่ 6 แขวงพญาไท เขตพญาไท กรุงเทพฯ 10400</p>
-          <div class="company-contact">โทร. 02-271-4343 &nbsp;&nbsp; แฟกซ์ 02-618-4530</div>
+          <div class="company-contact">โทร. 02-271-4343 &nbsp;&nbsp; แฟกซ์ 02-618-4530 &nbsp;&nbsp;www.cropsciences.co.th </div>
         </div>
       </div>
 
