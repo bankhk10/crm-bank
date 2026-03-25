2582a5fe45526b01b27f0a70c354a7b217b0050b
ATTHAPOL\Atthapol <Atthapol>
2026-03-25 10:54:28 +0700

    add

--- modules/layout/constants.tsx
+++ modules/layout/constants.tsx
@@ -95,7 +95,7 @@ export const navigationItems: SidebarNavItem[] = [
     children: [
       { href: "/customers", label: "ข้อมูลลูกค้า" },
       { href: "/credit-limits", label: "วงเงินเครดิต", permissionKey: "menu.credit_limits" },
-      { href: "/temporary-credit-limits", label: "วงเงินเครดิตชั่วคราว", permissionKey: "menu.temporary_credit_limits" },
+      // { href: "/temporary-credit-limits", label: "วงเงินเครดิตชั่วคราว", permissionKey: "menu.temporary_credit_limits" },
     ],
   },
   {
