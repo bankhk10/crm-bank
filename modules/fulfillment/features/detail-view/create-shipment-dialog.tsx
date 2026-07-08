b761e944fa793ac447f09971af8fa6e08b2bc30e
ATTHAPOL\Atthapol <Atthapol>
2026-07-08 11:58:33 +0700

    add

--- modules/fulfillment/features/detail-view/create-shipment-dialog.tsx
+++ modules/fulfillment/features/detail-view/create-shipment-dialog.tsx
@@ -312,7 +312,7 @@ export function CreateShipmentDialog({
         )}
       </DialogTrigger>
 
-      <DialogContent className="max-w-[120vw] overflow-y-auto sm:max-w-4xl">
+      <DialogContent className="max-w-[120vw] max-h-[90vh] overflow-y-auto sm:max-w-4xl">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             {isEdit ? (
