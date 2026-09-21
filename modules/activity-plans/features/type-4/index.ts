9078ec19aad8b598a33997a672d93f61c0b8c244
ATTHAPOL\Atthapol <Atthapol>
2026-09-21 08:44:21 +0700

    refactor(activity-plans): transition TYPE-4 to create/ and edit/ structure

--- modules/activity-plans/features/type-4/index.ts
+++ modules/activity-plans/features/type-4/index.ts
@@ -1,4 +1,11 @@
-export { Type4Collect } from "./form/type4-collect";
+export {
+  Type4Collect,
+  type CustomerOption,
+} from "./create/type4-collect";
+export {
+  EditType4Collect,
+  type EditCustomerOption,
+} from "./edit/edit-type4";
 export { DetailType4Collect } from "./detail/detail-type4-collect";
 export { ApprovalType4Collect } from "./approval/approval-type4-collect";
 export { ActualType4Collect } from "./actual/actual-type4-collect";
