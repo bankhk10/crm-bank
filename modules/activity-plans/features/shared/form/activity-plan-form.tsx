0d090e99af09c892860f4a5253e2051e45b14bf7
ATTHAPOL\Atthapol <Atthapol>
2026-09-21 08:49:30 +0700

    refactor(activity-plans): transition TYPE-8 to create/ and edit/ structure

--- modules/activity-plans/features/shared/form/activity-plan-form.tsx
+++ modules/activity-plans/features/shared/form/activity-plan-form.tsx
@@ -142,7 +142,7 @@ import { Type4Collect } from "@/modules/activity-plans/features/type-4/create/ty
 import { Type5Survey } from "@/modules/activity-plans/features/type-5/create/type5-survey";
 import { Type6Issue } from "@/modules/activity-plans/features/type-6/create/type6-issue";
 import { Type7Demo } from "./components/work-types/type7-demo";
-import { Type8Meeting } from "@/modules/activity-plans/features/type-8/form/type8-meeting";
+import { Type8Meeting } from "@/modules/activity-plans/features/type-8/create/type8-meeting";
 import { Type9Store } from "@/modules/activity-plans/features/type-9/form/type9-store";
 import { Type10FieldDay } from "@/modules/activity-plans/features/type-10/form/type10-field-day";
 import { Type11Stock } from "@/modules/activity-plans/features/type-11/form/type11-stock";
--- modules/activity-plans/features/type-8/index.ts
+++ modules/activity-plans/features/type-8/index.ts
@@ -1,4 +1,11 @@
-export { Type8Meeting } from "./form/type8-meeting";
+export {
+  Type8Meeting,
+  type ProductOption,
+} from "./create/type8-meeting";
+export {
+  EditType8Meeting,
+  type EditProductOption,
+} from "./edit/edit-type8";
 export { DetailType8Meeting } from "./detail/detail-type8-meeting";
 export { ApprovalType8Meeting } from "./approval/approval-type8-meeting";
 export { ActualType8Meeting } from "./actual/actual-type8-meeting";
